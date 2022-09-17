6b55fce8a78b3b230b0e24cf8dc0070ae70fc608
Luiz <lazevedotec.lf@gmail.com>
2022-09-16 21:06:44 -0300

    fix[Corrreção do login]

--- ponto_online_app/routes/auth.py
+++ ponto_online_app/routes/auth.py
@@ -1,10 +1,7 @@
 from ponto_online_app import app, bcrypt
 from flask import request, redirect, flash, url_for, session
 
-from ponto_online_app.models.users_model import Users
-from ponto_online_app.models.employees_model import Employees
-
-from ponto_online_app.database.db_session import create_session
+from ponto_online_app.services.bd_read import read_user_single_session, read_employees_single_session
 
 
 @app.route('/auth', methods=['POST'])
@@ -13,19 +10,22 @@ def authenticate():
     usuario = request.form['acess']
     password = request.form['password']
 
-    if find_acess is None:
+    find_acess_user = read_user_single_session(usuario)
+    find_acess_employees = read_employees_single_session(usuario)
+
+    if find_acess_user[0] is None:
         if find_acess_employees is None:
             flash('Usuário não encontrado')
             return redirect(url_for('login'))
 
-        elif bcrypt.check_password_hash(find_acess_employees.password, password) is False:
+        elif bcrypt.check_password_hash(find_acess_employees[1], password) is False:
             flash('Senha incorreta')
             return redirect(url_for('login'))
 
         session['funcionario_logado'] = usuario
         return redirect(url_for('index'))
 
-    elif bcrypt.check_password_hash(find_acess.password, password) is False:
+    elif bcrypt.check_password_hash(find_acess_user[1], password) is False:
         flash('Senha incorreta')
         return redirect(url_for('login'))
 
--- ponto_online_app/services/bd_read.py
+++ ponto_online_app/services/bd_read.py
@@ -1,8 +1,10 @@
 from ponto_online_app.database.db_session import create_session
 
+# Models
 from ponto_online_app.models.point_model import Point
 from ponto_online_app.models.users_model import Users
 from ponto_online_app.models.employees_model import Employees
+
 
 def read_user_session(email: str):
 
@@ -15,10 +17,28 @@ def read_user_single_session(email: str):
 
     with create_session() as session_db:
         user = session_db.query(Users).filter(Users.email == email).first()
+        password_user = user.password
         
-        return user
-        
-        #find_acess_employees = session_db.query(Employees).filter(Employees.cpf_id == usuario).first()
+        return user, password_user
+
+
+def read_employees_session(email: str):
+
+    with create_session() as session_db:
+        employees = session_db.query(Employees).filter(Employees.email == email)
+        return [employees]
+
+
+def read_employees_single_session(email: str):
+
+    with create_session() as session_db:
+        employees = session_db.query(Employees).filter(Employees.email == email).first()
+
+        password_employees = employees.password
+
+        return employees, password_employees
+
+# find_acess_employees = session_db.query(Employees).filter(Employees.cpf_id == usuario).first()
 
 
 def read_point(cpf: str, date: str):
